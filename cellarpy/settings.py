f1c949be1570aad9a0d9e459b179599a7c69cb46
Jonathan Huot <jonathan.huot@gmail.com>
2015-01-31 22:22:24 +0100

    Initial revision including basic logging and basic config file.

---
+++ cellarpy/settings.py
@@ -0,0 +1,58 @@
+import logging
+import os
+from os import path
+
+
+def setwritabledirectory(folders):
+    def iswritable(directory):
+        if not path.exists(directory):
+            try:
+                os.makedirs(directory)
+            except:
+                return False
+        return os.access(directory, os.W_OK | os.X_OK | os.R_OK)
+
+    for dir in folders:
+        if(iswritable(dir)):
+            return dir
+    logging.debug("No writable dir was found amongst: " + str(folders))
+    raise Exception("No writable dir was found amongst: " + str(folders))
+
+
+def setreadablefile(folders, filename):
+    def isreadable(directory, filename):
+        if path.isfile(path.join(directory, filename)):
+            return os.access(path.join(directory, filename), os.R_OK)
+        return False
+
+    for dir in folders:
+        if(isreadable(dir, filename)):
+            return path.join(dir, filename)
+    logging.debug("No readable conf file was found amongst: " + str(folders))
+    raise Exception("No readable conf file was found amongst: " + str(folders))
+
+
+# Initiate logging
+def load_logfile(logfile, extradirs=[]):
+    log_dir = setwritabledirectory(extradirs + [path.dirname(path.realpath(__file__))])
+    logging.basicConfig(level=logging.DEBUG,
+                        filename=path.join(log_dir, logfile),
+                        format='%(asctime)s +%(msecs)d [%(process)d] %(levelname)s %(message)s',
+                        datefmt='%d/%b/%Y:%H:%M:%S')
+    logging.debug('Log file start')
+
+
+def load_settings(settingsfile, extradirs=[]):
+    config_file = setreadablefile(extradirs + [path.join(path.dirname(path.realpath(__file__)))], settingsfile)
+    with open(config_file) as fd:
+        import json
+        settings = json.load(fd)
+
+    if "storage" in settings:
+        for name, folder in settings["storage"].iteritems():
+            settings["storage"][name] = setwritabledirectory([folder, path.join(path.dirname(path.realpath(__file__)), name)])
+            if name == "database" and settings["sqlite"][name][0] != '/':
+                settings["sqlite"][name] = path.join(settings["storage"][name], settings["sqlite"][name])
+
+    logging.debug('Settings loaded')
+    return settings
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "cellarpy",
+    version = "0.1",
+    author = "Jonathan Huot",
+    author_email = "jonathan.huot@gmail.com",
+    description = ("cellarpy empower python web-applications based on bottlepy"),
+    license = "MIT",
+    keywords = "bottle web wsgi",
+    url = "https://github.com/JonathanHuot/cellarpy",
+    packages=['cellarpy'],
+    long_description=read('README'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
